08bc680c3e4cbfcdbd7ec29b762b9f3dddc67b83
Hasan Pourghannad <mhpourghannad@gmail.com>
2025-10-28 15:27:41 +0330

    add server side request

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,8 +3,8 @@ import { Button } from "@heroui/button";
 export default function Header() {
   return (
     <header className="w-full flex items-center justify-between py-3">
+      <Button>ورود\ثبت‌نام</Button>
       <h1 className="text-3xl font-bold">اچ‌تی‌ام‌الی‌ها</h1>
-      <Button color="primary">ورود\ثبت‌نام</Button>
     </header>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,30 @@
 import DefaultLayout from "@/layouts/default";
 
-export default function IndexPage() {
+export const getServerSideProps = async () => {
+  const res = await fetch(
+    `https://newsapi.org/v2/everything?q=javascript&pageSize=20&sortBy=publishedAt&apiKey=${process.env.NEWS_API_TOKEN}&sortBy=popularity&language=en`,
+  );
+  const data = await res.json();
+
+  return { props: { data } };
+};
+
+interface IndexProps {
+  data: any;
+}
+
+export default function IndexPage({ data }: IndexProps) {
   return (
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         HTMLIHA
+        {data?.articles?.map((item, index) => {
+          return (
+            <div key={index}>
+              <h4>{item.title}</h4>
+            </div>
+          );
+        })}
       </section>
     </DefaultLayout>
   );
